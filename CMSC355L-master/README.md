4eaf1c73ce6b7ff5a74f048288b370d79f583122
barneshn2 <70536857+barneshn2@users.noreply.github.com>
2020-09-20 15:22:43 -0400

    Add files via upload

---
+++ CMSC355L-master/README.md
@@ -0,0 +1,6 @@
+# Project Name(for now): Weekly Meal Planner
+# Shiza Khan
+# Sabrina Khan
+# Trevon Lamb 
+# Hailey Barnes
+# Karim 
---
+++ CMSC355L-master/app/src/main/java/com/example/groupl/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.groupl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void AddaPlan(View view) {
+    }
+
+    public void DeleteaPlan(View view) {
+    }
+}
